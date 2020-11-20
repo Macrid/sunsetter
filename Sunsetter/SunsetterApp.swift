6bcaf15e5f50cf6acb432e00200e943d3aa2b6d3
Erik Persson <erikpersson96@hotmail.com>
2020-11-20 18:44:49 +0100

    Initial Commit

---
+++ Sunsetter/SunsetterApp.swift
@@ -0,0 +1,17 @@
+//
+//  SunsetterApp.swift
+//  Sunsetter
+//
+//  Created by Erik Persson on 2020-11-20.
+//
+
+import SwiftUI
+
+@main
+struct SunsetterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
